f4c6d771a0a5d507f21b1863fc89ddaa69558cc6
Richard Mands <richard@eduku.org>
2017-07-10 15:18:01 +0100

    update

--- src/components/MiniProject.js
+++ src/components/MiniProject.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router';
-import SkyTest from './SkyTest'
+import WaymarkTest from './WaymarkTest'
 
 const MiniProject = () => {
   return (
     <div className="section1Box">
       <div className="section1">
-        <h2 className="titleText">SkyEurope Test</h2>
+        <h2 className="titleText">WaymarkIT Test</h2>
         {/* <p className="bodyText">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p> */}
-        <SkyTest/>
+        <WaymarkTest/>
       </div>
     </div>
       );
---
+++ src/components/WayMarkTest.js
@@ -0,0 +1,316 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import jsonp from 'jsonp';
+import $ from 'jquery';
+
+import * as WaymarkActions from '../actions/WaymarkActions';
+
+class WaymarkTest extends Component {
+
+  constructor(){
+    super()
+    this.state = { overTen : false,
+      data : [
+    {
+      "Name": "lada",
+      "Id": 1,
+      "Age": 1
+    },
+    {
+      "Name": "lava",
+      "Id": 2,
+      "Age": 15
+    },
+    {
+      "Name": "lhannah",
+      "Id": 3,
+      "Age": 16
+    },
+    {
+      "Name": "lotto",
+      "Id": 4,
+      "Age": 9
+    },
+    {
+      "Name": "lanna",
+      "Id": 5,
+      "Age": 18
+    },
+    {
+      "Name": "reinier",
+      "Id": 6,
+      "Age": 20
+    },
+    {
+      "Name": "maham",
+      "Id": 7,
+      "Age": 22
+    },
+    {
+      "Name": "sahas",
+      "Id": 8,
+      "Age": 24
+    },
+    {
+      "Name": "emme",
+      "Id": 9,
+      "Age": 41
+    },
+    {
+      "Name": "aviva",
+      "Id": 10,
+      "Age": 8
+    }
+  ],
+  dataOriginal : [
+  {
+    "Name": "lada",
+    "Id": 1,
+    "Age": 1
+  },
+  {
+    "Name": "lava",
+    "Id": 2,
+    "Age": 15
+  },
+  {
+    "Name": "lhannah",
+    "Id": 3,
+    "Age": 16
+  },
+  {
+    "Name": "otto",
+    "Id": 4,
+    "Age": 9
+  },
+  {
+    "Name": "lanna",
+    "Id": 5,
+    "Age": 18
+  },
+  {
+    "Name": "reinier",
+    "Id": 6,
+    "Age": 20
+  },
+  {
+    "Name": "maham",
+    "Id": 7,
+    "Age": 22
+  },
+  {
+    "Name": "sahas",
+    "Id": 8,
+    "Age": 24
+  },
+  {
+    "Name": "emme",
+    "Id": 9,
+    "Age": 41
+  },
+  {
+    "Name": "aviva",
+    "Id": 10,
+    "Age": 8
+  }
+]}
+    this.reverseNames = this.reverseNames.bind(this)
+    this.sortByAge = this.sortByAge.bind(this)
+    this.reset = this.reset.bind(this)
+    this.olderThanTen = this.olderThanTen.bind(this)
+    this.saveFile = this.saveFile.bind(this)
+  }
+
+  reverseNames(data){
+    let newData = data.map(person => {
+      console.log('person: ', person)
+      let name = person.Name
+      let arr = name.split('');
+      let reverseArray = arr.reverse();
+      let revString = reverseArray.join('')
+      person.Name = revString
+      return person
+    })
+    this.setState ({
+      data: newData
+    })
+  }
+
+  sortByAge(data){
+    let newData = data.sort((a,b) => {
+      // console.log('a.Age: ', a.Age)
+      return a.Age - b.Age
+      // parseInt(a.Age, 10) - parseInt(b.Age, 10)
+    })
+    this.setState ({
+      data: newData
+    })
+  }
+  olderThanTen(){
+    if (this.state.overTen === true){
+      this.setState ({
+        overTen: false
+      })
+    } else
+    {
+      this.setState ({
+        overTen: true
+      })
+    }
+  }
+
+  saveFile(data){
+    console.log('save file');
+    let result
+    function convertArrayOfObjectsToCSV(args) {
+        var ctr, keys, columnDelimiter, lineDelimiter;
+
+        // data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+        console.log('result: ', result)
+        return result;
+    }
+
+    convertArrayOfObjectsToCSV(data)
+    downloadCSV(result)
+
+    function downloadCSV(result) {
+        var filename, link;
+        var csv = convertArrayOfObjectsToCSV({
+            data: result
+        });
+        if (csv == null) return;
+
+        filename = 'FEFIe123.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    }
+
+  }
+
+  reset(){
+    this.setState({
+      data : this.state.dataOriginal
+    })
+  }
+
+  render() {
+    const { } = this.props;
+    const { data, dataNew } = this.state;
+    console.log('data: ', data)
+
+    // axios.get('https://techtestpersonapi.azurewebsites.net/api/GETPersonsTechTestAPI?code=Z5Dm297Ijn9weSo75EVtsJHN9HoVE0fgJt8zIGXWV4ZOOCGNpaYBtw==')
+    // .then((err, res) => {
+    //   console.log('res: ', res)
+    // })
+    //
+    // $.get(`https://techtestpersonapi.azurewebsites.net/api/GETPersonsTechTestAPI?code=Z5Dm297Ijn9weSo75EVtsJHN9HoVE0fgJt8zIGXWV4ZOOCGNpaYBtw==` , response => {
+    //   console.log('response: ', response)
+    // })
+
+
+let displayData
+if (this.state.overTen === false){
+  displayData = data.map(person => {
+    return(
+      <div className="dataEach" key={person.Id}>
+        Name : {person.Name} <br/>
+        Id : {person.Id} <br/>
+        Age : {person.Age} <br/>
+      </div>
+    )
+  })
+} else {
+  displayData = data.map(person => {
+    if (person.Age > 10){
+      return(
+        <div className="dataEachOverTen" key={person.Id}>
+          Name : {person.Name} <br/>
+          Id : {person.Id} <br/>
+          Age : {person.Age} <br/>
+        </div>
+      )
+    } else {
+      return(
+        <div className="dataEach" key={person.Id}>
+          Name : {person.Name} <br/>
+          Id : {person.Id} <br/>
+          Age : {person.Age} <br/>
+        </div>
+      )
+    }
+  })
+}
+
+    return (
+      <div className="waymarkBox">
+        <div className="headerText">
+          Data
+        </div>
+        <div className="buttonBox">
+          <div className="waymarkButton" onClick={() => this.reverseNames(data)}>
+            <div className="waymarkButtonText">Reverse Names</div>
+          </div>
+          <div className="waymarkButton" onClick={() => this.sortByAge(data)}>
+            <div className="waymarkButtonText">Sort by Age</div>
+          </div>
+          <div className="waymarkButton" onClick={() => this.olderThanTen()}>
+            <div className="waymarkButtonText">Older Than 10</div>
+          </div>
+          <div className="waymarkButton" onClick={() => this.saveFile(data)}>
+            <div className="waymarkButtonText">Save File</div>
+          </div>
+        </div>
+        <div className="dataDisplay">
+          {displayData}
+        </div>
+        <div className="buttonBox">
+          <div className="waymarkButtonReset" onClick={() => this.reset(data)}>
+            <div className="waymarkButtonText">Reset</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WaymarkTest);
